42572a5d9a45c773d66fbc92a2497b878816568c
FabriDamazio <fabridamazio@gmail.com>
2023-02-16 15:58:20 -0300

    Button sizes and rounded borders

--- site/lib/site_web/components/componex_material/components.ex
+++ site/lib/site_web/components/componex_material/components.ex
@@ -4,6 +4,8 @@ defmodule ComponexMaterial.Components do#{@color}
   attr(:type, :string, default: nil)
   attr(:color, :string, default: "purple")
   attr(:variant, :string, default: "filled")
+  attr(:size, :string, default: "medium")
+  attr(:rounded, :string, default: "rounded")
   attr(:class, :string, default: nil)
   attr(:rest, :global, include: ~w(disabled form name value))
 
@@ -14,10 +16,17 @@ defmodule ComponexMaterial.Components do#{@color}
     <button
       type={@type}
       class={[
-        @variant == "elevated" && "middle none center mr-3 rounded-full bg-#{@color}-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-#{@color}-500/20 transition-all hover:shadow-lg hover:shadow-#{@color}-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none",
-        @variant == "filled" && "middle none center mr-3 rounded-full bg-#{@color}-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-#{@color}-500/20 transition-all hover:shadow-lg hover:shadow-#{@color}-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none",
-        @variant == "outline" && "middle none center mr-3 rounded-full border border-#{@color}-500 py-3 px-6 font-sans text-xs font-bold uppercase text-#{@color}-500 transition-all hover:opacity-75 focus:ring focus:ring-#{@color}-200 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none",
-        @variant == "text" && "middle none center rounded-full py-3 px-6 font-sans text-xs font-bold uppercase text-#{@color}-500 transition-all hover:bg-#{@color}-500/10 active:bg-#{@color}-500/30 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none",
+        @size == "small" && "py-2 px-4 text-xs",
+        @size == "medium" && "py-3 px-6 text-xs",
+        @size == "large" && "py-3.5 px-7 text-sm",
+        @rounded == "rounded" && "rounded",
+        @rounded == "md" && "rounded-md",
+        @rounded == "lg" && "rounded-lg",
+        @rounded == "full" && "rounded-full",
+        @variant == "elevated" && "middle none center bg-#{@color}-500 font-sans text-xs font-bold uppercase text-white shadow-md shadow-#{@color}-500/20 transition-all hover:shadow-lg hover:shadow-#{@color}-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none",
+        @variant == "filled" && "middle none center bg-#{@color}-500 font-sans text-xs font-bold uppercase text-white shadow-md shadow-#{@color}-500/20 transition-all hover:shadow-lg hover:shadow-#{@color}-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none",
+        @variant == "outline" && "middle none center border border-#{@color}-500 font-sans text-xs font-bold uppercase text-#{@color}-500 transition-all hover:opacity-75 focus:ring focus:ring-#{@color}-200 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none",
+        @variant == "text" && "middle none center font-sans text-xs font-bold uppercase text-#{@color}-500 transition-all hover:bg-#{@color}-500/10 active:bg-#{@color}-500/30 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none",
         @class
       ]}
       {@rest}
