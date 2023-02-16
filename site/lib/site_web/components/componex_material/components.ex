11d986ac7c91a2fabac8651348007a3ed6ff9deb
FabriDamazio <fabridamazio@gmail.com>
2023-02-16 16:47:59 -0300

    Swap button elevated and filled styles

--- site/lib/site_web/components/componex_material/components.ex
+++ site/lib/site_web/components/componex_material/components.ex
@@ -25,8 +25,8 @@ defmodule ComponexMaterial.Components do#{@color}
         @rounded == "lg" && "rounded-lg",
         @rounded == "full" && "rounded-full",
         @block_level == true && "w-full",
-        @variant == "elevated" && "middle none center bg-#{@color}-500 font-sans text-xs font-bold uppercase text-white shadow-md shadow-#{@color}-500/20 transition-all hover:shadow-lg hover:shadow-#{@color}-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none",
-        @variant == "filled" && "middle none center bg-#{@color}-500 font-sans text-xs font-bold uppercase text-white shadow-md shadow-#{@color}-500/20 transition-all hover:shadow-lg hover:shadow-#{@color}-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none",
+        @variant == "elevated" && "middle none center bg-#{@color}-500 font-sans text-xs font-bold uppercase text-white shadow-md shadow-#{@color}-500/20 transition-all hover:shadow-lg hover:shadow-#{@color}-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none",
+        @variant == "filled" && "middle none center bg-#{@color}-500 font-sans text-xs font-bold uppercase text-white transition-all hover:shadow-sm hover:opacity-75 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none",
         @variant == "outline" && "middle none center border border-#{@color}-500 font-sans text-xs font-bold uppercase text-#{@color}-500 transition-all hover:opacity-75 focus:ring focus:ring-#{@color}-200 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none",
         @variant == "text" && "middle none center font-sans text-xs font-bold uppercase text-#{@color}-500 transition-all hover:bg-#{@color}-500/10 active:bg-#{@color}-500/30 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none",
         @class
